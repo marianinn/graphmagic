c0c06d093522722a2127d756094a0fbcbfc14e63
dlazerka <dlazerka@01eb5bb2-106f-11de-a058-67ab0bfb5968>
2009-02-16 19:38:07 +0000

    Basic painting is done.

--- src/main/java/name/dlazerka/gc/model/Graph.java
+++ src/main/java/name/dlazerka/gc/model/Graph.java
@@ -2,7 +2,9 @@ package name.dlazerka.gc.model;
 
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
 
 /**
  * @author Dzmitry Lazerka
@@ -10,15 +12,35 @@ import java.util.Set;
 public class Graph {
 	private final Set<Vertex> vertexSet = new HashSet<Vertex>();
 	private final Set<Arc> arcSet = new HashSet<Arc>();
+	private final Map<Integer, Vertex> vertexByOrder = new HashMap<Integer, Vertex>();
 
 	public Graph() {
 		Vertex vertex1 = new Vertex(1);
-//		Vertex vertex2 = new Vertex(2);
-		vertexSet.add(vertex1);
-//		vertexSet.add(vertex2);
+		Vertex vertex2 = new Vertex(2);
+		Vertex vertex3 = new Vertex(3);
+		Vertex vertex4 = new Vertex(4);
 
+		addVertex(vertex1);
+		addVertex(vertex2);
+		addVertex(vertex3);
+		addVertex(vertex4);
 
-//		arcSet.add(new Arc(vertex1, vertex2));
+		Arc arc1 = new Arc(vertex1, vertex2);
+		Arc arc2 = new Arc(vertex2, vertex3);
+		Arc arc3 = new Arc(vertex3, vertex1);
+
+		addArc(arc1);
+		addArc(arc2);
+		addArc(arc3);
+	}
+
+	private void addArc(Arc arc) {
+		arcSet.add(arc);
+	}
+
+	public void addVertex(Vertex vertex) {
+		vertexByOrder.put(vertexSet.size(), vertex);
+		vertexSet.add(vertex);
 	}
 
 	public Set<Vertex> getVertexSet() {
@@ -29,5 +51,7 @@ public class Graph {
 		return arcSet;
 	}
 
-	
+	public Vertex getVertexByOrder(int i) {
+		return vertexByOrder.get(i);
+	}
 }
