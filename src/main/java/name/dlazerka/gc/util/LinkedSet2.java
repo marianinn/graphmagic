3531b175dd0824ec19a66cd01847612c31d45b83
dlazerka <dlazerka@01eb5bb2-106f-11de-a058-67ab0bfb5968>
2009-02-20 15:44:15 +0000

    Fixed z-index when moving.

--- src/main/java/name/dlazerka/gc/util/LinkedSet2.java
+++ src/main/java/name/dlazerka/gc/util/LinkedSet2.java
@@ -7,36 +7,36 @@ import java.util.Set;
 /**
  * @author Dzmitry Lazerka
  */
-public class LinkedSet2<T> extends LinkedList<T> implements Set<T>, List<T> {
+public class LinkedSet2<E> extends LinkedList<E> implements Set<E>, List<E> {
 	@Override
-	public void addFirst(T t) {
-		if (!contains(t)) {
-			super.addFirst(t);
+	public void addFirst(E e) {
+		if (!contains(e)) {
+			super.addFirst(e);
 		}
 	}
 
 	@Override
-	public void addLast(T t) {
-		if (!contains(t)) {
-			super.addLast(t);
+	public void addLast(E e) {
+		if (!contains(e)) {
+			super.addLast(e);
 		}
 	}
 
 	@Override
-	public boolean add(T t) {
-		if (!contains(t)) {
-			return super.add(t);
+	public boolean add(E e) {
+		if (!contains(e)) {
+			return super.add(e);
 		}
 		return false;
 	}
 
 	@Override
-	public T set(int index, T element) {
+	public E set(int index, E element) {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public void add(int index, T element) {
+	public void add(int index, E element) {
 		throw new UnsupportedOperationException();
 	}
 }
