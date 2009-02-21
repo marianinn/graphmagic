955cdb49eaae1710be4a2ee1f55df2c7a2b3196d
dlazerka <dlazerka@01eb5bb2-106f-11de-a058-67ab0bfb5968>
2009-02-21 15:33:26 +0000

    Vertices now represented by JPanel-s. Moving refactored.

--- trunk/src/main/java/name/dlazerka/gc/ui/Draggable.java
+++ trunk/src/main/java/name/dlazerka/gc/ui/Draggable.java
@@ -1,14 +1,10 @@
 package name.dlazerka.gc.ui;
 
-import java.awt.*;
-import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 /**
  * @author Dzmitry Lazerka
  */
-public interface Draggable extends MouseMotionListener {
-	public boolean contains(Point point);
-	public void startFollowingMouse(MouseEvent e);
-	public void stopFollowingMouse(MouseEvent e);
+public interface Draggable extends MouseListener, MouseMotionListener {
 }
--- trunk/src/main/java/name/dlazerka/gc/ui/GraphPanelModel.java
+++ trunk/src/main/java/name/dlazerka/gc/ui/GraphPanelModel.java
@@ -1,13 +1,12 @@
 package name.dlazerka.gc.ui;
 
 import name.dlazerka.gc.bean.Graph;
-import name.dlazerka.gc.bean.Vertex;
-import name.dlazerka.gc.bean.Edge;
 import name.dlazerka.gc.bean.GraphChangeListener;
-import name.dlazerka.gc.util.LinkedSet;
+import name.dlazerka.gc.bean.Vertex;
 
-import java.util.*;
 import java.awt.*;
+import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * @author Dzmitry Lazerka
@@ -15,87 +14,29 @@ import java.awt.*;
 public class GraphPanelModel implements GraphChangeListener {
 	private final Graph graph;
 
-	/**
-	 * Order represents Z-index.
-	 */
-	protected final LinkedSet<VertexUI> vertexUISet = new LinkedSet<VertexUI>();
-
-	/**
-	 * Order represents Z-index.
-	 */
-	protected final LinkedSet<EdgeUI> edgeUISet = new LinkedSet<EdgeUI>();
-
-	private final Map<Vertex, VertexUI> vertexToVertexUI = new HashMap<Vertex, VertexUI>();
-	private final Map<Edge, EdgeUI> edgeToEdgeUI = new HashMap<Edge, EdgeUI>();
-
 	protected Collection<Draggable> draggingObjects = new LinkedList<Draggable>();
 
 	public GraphPanelModel(Dimension size) {
 		graph = new Graph();
 		graph.addChangeListener(this);
-
-		createUIs(size);
-	}
-
-	public void vertexAdded(Vertex vertex) {
-		VertexUI vertexUI = new VertexUI(vertex);
-		addVertexUI(vertexUI);
 	}
 
 	public void addVertex() {
 		graph.addVertex();
 	}
 
-	public boolean addVertexUI(VertexUI vertexUI) {
-		return vertexUISet.add(vertexUI);
-	}
-
-	private void createUIs(Dimension size) {
-		for (Vertex vertex : graph.getVertexSet()) {
-			VertexUI vertexUI = new VertexUI(vertex);
-//			add(vertexUI);
-			vertexUISet.add(vertexUI);
-			vertexToVertexUI.put(vertex, vertexUI);
-		}
-		for (Edge edge : graph.getEdgeSet()) {
-			EdgeUI edgeUI = new EdgeUI(
-				edge, vertexToVertexUI.get(edge.getHead()), vertexToVertexUI.get(edge.getTail())
-			);
-			edgeUISet.add(edgeUI);
-//			edgeToEdgeUI.put(edge, edgeUI);
-		}
-
-		calculateVertexPositions(size);
-	}
-
-	private void calculateVertexPositions(Dimension size) {
-		int vertexSetSize = vertexToVertexUI.size();
-
-		Point center = new Point(size.width / 2, size.height / 2);
-		int radius = Math.min(size.width * 3 / 4 - center.x, size.height * 3 / 4 - center.y);
-
-		double angleStep = 2 * Math.PI / vertexSetSize;
-
-		int i = 0;
-		for (VertexUI vertexUI : vertexToVertexUI.values()) {
-			double angle = i * angleStep;
-
-			Point position = new Point(
-				(int) Math.round(radius * Math.cos(angle)) + center.x,
-				(int) Math.round(radius * -Math.sin(angle)) + center.y
-			);
-
-			vertexUI.setCenter(position);
-			i++;
-		}
+	public Graph getGraph() {
+		return graph;
 	}
 
 	/**
-	 * Iterates through {@link #vertexUISet} and checks if any of them {@link VertexUI#contains(Point)}
+	 * Iterates through {@link #vertexModelSet} and checks if any of them {@link VertexPanelModel#contains(Point)}
 	 *
 	 * @param point point under which to search
-	 * @return a {@link VertexUI} or null of not found.
+	 * @return a {@link VertexPanelModel} or null of not found.
+	 *
 	 */
+	/*
 	protected VertexUI getVertexUIUnder(Point point) {
 		LinkedSet<VertexUI> set = vertexUISet;
 		ListIterator<VertexUI> listIterator = set.listIterator(set.size());
@@ -106,5 +47,9 @@ public class GraphPanelModel implements GraphChangeListener {
 			}
 		}
 		return null;
+	}
+	*/
+	
+	public void vertexAdded(Vertex vertex) {
 	}
 }
