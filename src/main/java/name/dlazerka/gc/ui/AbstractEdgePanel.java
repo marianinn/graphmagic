ecc1f7ed7285ae475a6a22a2ce756f801db1e036
dlazerka <dlazerka@01eb5bb2-106f-11de-a058-67ab0bfb5968>
2009-03-09 00:12:42 +0000

    Some tips for curve edges (unimportant feature).

--- src/main/java/name/dlazerka/gc/ui/AbstractEdgePanel.java
+++ src/main/java/name/dlazerka/gc/ui/AbstractEdgePanel.java
@@ -14,16 +14,16 @@ public abstract class AbstractEdgePanel extends JPanel {
 	private static final Logger logger = LoggerFactory.getLogger(AbstractEdgePanel.class);
 
 	private static final Color EDGE_COLOR = Color.BLACK;
-	private static final Stroke EDGE_STROKE = new BasicStroke(1.1f);
+	private static final Stroke EDGE_STROKE = new BasicStroke(2f);
 	private final QuadCurve2D curve = new QuadCurve2D.Double();
 	private Shape lastShownShape = curve;
 
-	protected void drawEdge(Graphics2D g, Point from, Point to) {
+	protected void drawEdge(Graphics2D g, Point from, Point ctrl, Point to) {
 		g.setColor(EDGE_COLOR);
 		g.setStroke(EDGE_STROKE);
 
 //		Point point = new Point((to.x + from.x) / 2, (to.y + from.y) / 2);
-		curve.setCurve(from, from, to);
+		curve.setCurve(from, ctrl, to);
 
 		lastShownShape = curve;
 		lastShownShape = EDGE_STROKE.createStrokedShape(lastShownShape);
