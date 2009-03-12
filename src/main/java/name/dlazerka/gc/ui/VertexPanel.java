74877a6c51261bb9a3cb40d10a510de671ef7358
dlazerka <dlazerka@01eb5bb2-106f-11de-a058-67ab0bfb5968>
2009-03-12 22:15:44 +0000

    Added copyright.

--- src/main/java/name/dlazerka/gc/ui/VertexPanel.java
+++ src/main/java/name/dlazerka/gc/ui/VertexPanel.java
@@ -1,3 +1,23 @@
+/*
+ * GraphMagic, package for scientists working in graph theory.
+ * Copyright (C) 2009 Dzmitry Lazerka dlazerka@dlazerka.name
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Author: Dzmitry Lazerka dlazerka@dlazerka.name
+ */
+
 package name.dlazerka.gc.ui;
 
 import name.dlazerka.gc.bean.Vertex;
